f92b53579706cd60559b332f3974cb22ef4f9a3a
primadzakyhibatulloh <161608393+primadzakyhibatulloh@users.noreply.github.com>
2024-03-02 20:13:23 +0700

    Update LatihanUnit4.c

--- Unit 4/LatihanUnit4.c
+++ Unit 4/LatihanUnit4.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 
-int kelilingPersegi(int sisi){
-    return 4 * sisi ;
+int sisi;
+int kelilingPersegi(int sisi)
+{
+    return 4 * sisi;
 }
 
-int luasPersegi(int sisi){
-    return sisi * sisi ;
+int luasPersegi(int sisi)
+{
+    return sisi * sisi;
 }
 
-int volumeKubus(int sisi){
+int volumeKubus(int sisi)
+{
     return sisi * sisi * sisi;
 }
 
-int main (){
-    int sisi ;
-    
-    do {
-        printf("sisi persegi: ");
-        scanf("%d", &sisi) ;
-    } while (sisi <= 0) ;
+int main()
+{
+    printf("Panjang sisi: ");
+    scanf("%d", &sisi);
+    if (sisi > 0)
+    {
+        printf("Keliling Persegi: %d\n", kelilingPersegi(sisi));
+        printf("Luas persegi: %d\n", luasPersegi(sisi));
+        printf("Volume Kubus: %d", volumeKubus(sisi));
+    }
+    else
+    {
+        printf("Input tidak valid");
+    }
 
-    printf("Keliling persegi: %d\n", kelilingPersegi(sisi));
-    printf("Luas persegi: %d\n", luasPersegi(sisi));
-    printf("Volume kubus: %d\n", volumeKubus(sisi));
-    
     return 0;
 }
